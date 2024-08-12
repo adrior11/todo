47d97ed47809139dd39a2eb65511f8d44a1ace07
adrior11 <adrian.schneider11@gmail.com>
2024-08-12 14:00:08 +0200

    docs: add documentation for generate_lua_config macro

--- src/macros.rs
+++ src/macros.rs
@@ -1,3 +1,20 @@
+/// Generates a Lua configuration string from a list of key-value pairs.
+///
+/// # Parameters
+///
+/// - `$key`: The key for the configuration option, which should be a string.
+/// - `$value`: The value for the configuration option, which will be formatted into the Lua string. 
+///
+/// # Example
+///
+/// ```
+/// let lua_config = generate_lua_config!(
+///     "first_option" => true,
+///     "second_option" => 42,
+/// );
+///
+/// assert_eq!(lua_config, "config = {\n    backup_on_reset = true,\n    another_option = 42,\n}\n");
+/// ```
 #[macro_export]
 macro_rules! generate_lua_config {
     ($($key:expr => $value:expr),* $(,)?) => {
